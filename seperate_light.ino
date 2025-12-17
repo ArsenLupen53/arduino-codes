b0a205ccd08b2d7620ce6157f1dc87a6ca43c520
Ahmet Berk Güneli <124775535+ArsenLupen53@users.noreply.github.com>
2025-12-17 22:42:46 +0300

    Change button input mode and update LED control logic

--- seperate_light.ino
+++ seperate_light.ino
@@ -7,47 +7,39 @@
 #define BUTTON2 9
 
 void setup() {
-  // LED pinlerini çıkış olarak ayarla
+  // put your setup code here, to run once:
   pinMode(RED_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
 
-  // Butonları dahili pull-up direnci ile giriş olarak ayarla
-  // Bu modda butona basınca 0 (LOW), basmayınca 1 (HIGH) okunur.
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT_PULLUP);
+  pinMode(BUTTON1, INPUT);
+  pinMode(BUTTON2, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  // Buton durumlarını oku
-  int b1Durum = digitalRead(BUTTON1);
-  int b2Durum = digitalRead(BUTTON2);
+  // put your main code here, to run repeatedly:
 
-  // Seri ekranda kontrol etmek istersen:
-  // Serial.print("B1: "); Serial.print(b1Durum);
-  // Serial.print(" - B2: "); Serial.println(b2Durum);
-
-  if (b1Durum == LOW) { // Button 1'e basıldıysa
+  if (digitalRead(BUTTON1) == 1) {
     digitalWrite(RED_LED, HIGH);
     digitalWrite(YELLOW_LED, HIGH);
     digitalWrite(BLUE_LED, LOW);
     digitalWrite(GREEN_LED, LOW);
   }
-  else if (b2Durum == LOW) { // Button 2'ye basıldıysa
+
+  else if (digitalRead(BUTTON2) == 1) {
     digitalWrite(BLUE_LED, HIGH);
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(RED_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
   }
-  else { // Hiçbir butona basılmıyorsa her şeyi söndür
+
+  else {
     digitalWrite(RED_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(BLUE_LED, LOW);
     digitalWrite(GREEN_LED, LOW);
   }
-  
-  delay(10); // İşlemciyi yormamak ve kararlı okuma için küçük bir gecikme
 }
